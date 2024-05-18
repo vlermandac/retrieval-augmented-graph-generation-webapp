b7623aea82727808819e78d8b45869d626439769
Vicente Lermanda <vlermanda@MacBook-Air-2.local>
2024-05-18 16:56:59 -0400

    frontend fix

--- searcher/tests/test_kg_gen.py
+++ searcher/tests/test_kg_gen.py
@@ -15,7 +15,7 @@ class TestKG(unittest.TestCase):
 
     def test_chunks_to_triplet(self):
         # We assume that the elastic search index is already created
-        chunks = self.clients.elastic_search().search(index='moby_dick-chunk', size=30)
+        chunks = self.clients.elastic_search().search(index='capital_volume_1-chunk')
         triplets = self.kg_builder.chunk_to_triplets(self.clients.open_ai(), 'gpt-4o', chunks) # probar
         self.kg_builder.process_triplets(triplets)
         print(self.kg_builder.serialize())
